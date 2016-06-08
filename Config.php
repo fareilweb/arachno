80b0336f9e6320bb7f67d84f354005fde26560c6
Luca Cilfone <Luca Cilfone>
2016-06-08 20:50:29 +0200

    update config

--- Config.php
+++ Config.php
@@ -2,7 +2,7 @@
 
 // Switch the right configuration file to load
 
-require_once(__DIR__.'/Config-production.php');
-//require_once(__DIR__.'/Config-localhost.php');
+//require_once(__DIR__.'/Config-production.php');
+require_once(__DIR__.'/Config-localhost.php');
 
 
