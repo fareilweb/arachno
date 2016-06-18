b3f7e6ffb4995f3b44b2c8317d2ed497e5257731
lc <info@fareilweb.com>
2016-06-18 19:02:36 +0200

    working at shop

--- controllers/Shop.php
+++ controllers/Shop.php
@@ -2,15 +2,48 @@
 
 class Shop extends Controller
 {
- 
-    public function index($args=[])
-    {
-        
+    public function index($args){
+        $this->showProducts($args);
     }
     
-    public function showProducts()
+ 
+    public function showItems($args)
     {
+        // Get Args
+        $this->args = $args;
         
+        // Elaborate Content
+        
+        
+        
+        // Views
+        $this->menus["main_menu"] = $this->getModel('MenuModel')->selectMenuDataById(1);
+        $this->includeView('nav/main_menu', 'header-content');
+        $this->includeView('nav/lang_menu', 'footer-content');
+        $this->includeView('shop/items', 'main-content');       
+        $this->getView('pages/page_default');
+    
     }
+ 
+    
+    public function showItem($args)
+    {
+        // Get Args
+        $this->args = $args;
+        
+        // Elaborate Content
+        //TO DO - Get Item Data By The MOdel
+        //$item_model = $this->getModel('ShopItemModel');
+       
+        
+        // Views
+        $this->menus["main_menu"] = $this->getModel('MenuModel')->selectMenuDataById(1);
+        $this->includeView('nav/main_menu', 'header-content');
+        $this->includeView('nav/lang_menu', 'footer-content');
+        $this->includeView('shop/items', 'main-content');       
+        $this->getView('pages/page_default');
+    }
+ 
+    
     
 }
