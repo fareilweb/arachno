69529e24b00d5c6a14221e08f3a683dfb421c081
lc <info@fareilweb.com>
2016-06-18 12:28:57 +0200

    working on shop

--- Config.php
+++ Config.php
@@ -1,8 +1,12 @@
 <?php
-
 // Switch the right configuration file to load
+switch($_SERVER['HTTP_HOST'])
+{
+    case "www.dia-techshop.it":
+           require_once(__DIR__.'/Config-production.php');
+        break;
 
-
-require_once(__DIR__.'/Config-production.php');
-
-require_once(__DIR__.'/Config-localhost.php');
+    default:
+            require_once(__DIR__.'/Config-localhost.php');
+        break;
+}
