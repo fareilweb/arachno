5e699b40efc082f5a76e27c679fd42176a3cf281
lc <info@fareilweb.com>
2016-06-21 19:23:21 +0200

    working at shop

--- languages/en-GB.php
+++ languages/en-GB.php
@@ -5,6 +5,10 @@
  *==============================================================================*/
 class Lang
 {
+    /* Internal String (IMPORTANT For Run The App) */
+    public static $lang_id = 2;
+    public static $lang_internal_code = "en-GB";
+    
     /* Public Area ============================================================ */
     public static $select_language = 'Select Language';
     
@@ -23,6 +27,21 @@ class Lang
     
     /* Shop =================================================================== */
     public static $products_list_title = "Products";
-    
+        
+    /* Shop Item */
+    public static $item_status_true = "Yes";
+    public static $item_status_false = "No";
+    public static $lbl_item_id = "ID";
+    public static $lbl_item_code = "Code";
+    public static $lbl_item_category = "Category";
+    public static $lbl_item_status = "Available Item";
+    public static $lbl_item_stock = "Stock";
+    public static $lbl_item_price = "Price &euro;";
+    public static $lbl_item_discount = "Discount";
+    public static $lbl_item_title = "Title";
+    public static $lbl_item_weight = "Weight";
+    public static $lbl_item_colors = "Colors";
+    public static $lbl_item_short_desc = "Short Description";
+    public static $lbl_item_long_desc = "Description";
     
 }
--- languages/it-IT.php
+++ languages/it-IT.php
@@ -5,6 +5,10 @@
  *==============================================================================*/
 class Lang
 {
+    /* Internal String (IMPORTANT For Run The App) */
+    public static $lang_id = 1;
+    public static $lang_internal_code = "it-IT";
+    
     /* Public Area ============================================================ */
     public static $select_language = 'Seleziona la lingua';
     
@@ -23,5 +27,24 @@ class Lang
     
     /* Shop =================================================================== */
     public static $products_list_title = "Prodotti";
+    
+    /* Shop Item */
+    public static $item_status_true = "S&igrave;";
+    public static $item_status_false = "No";
+    public static $lbl_item_id = "ID";
+    public static $lbl_item_code = "Codice";
+    public static $lbl_item_category = "Categoria";
+    public static $lbl_item_status = "Oggetto Disponibile";
+    public static $lbl_item_stock = "Qt&agrave;. Magazzino";
+    public static $lbl_item_price = "Prezzo &euro;";
+    public static $lbl_item_discount = "Sconto";
+    public static $lbl_item_title = "Titolo";
+    public static $lbl_item_weight = "Peso";
+    public static $lbl_item_colors = "Colori";
+    public static $lbl_item_short_desc = "Riepilogo";
+    public static $lbl_item_long_desc = "Descrizione";
+    
+    
+    
     
 } 
