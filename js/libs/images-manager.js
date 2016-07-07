e19974c68ea14deaf71bf997fc826c29e7c31361
lc <info@fareilweb.com>
2016-07-07 10:39:49 +0200

    upd

--- js/libs/images-manager.js
+++ js/libs/images-manager.js
@@ -1,30 +1,21 @@
-class persona {
- 
-    constructor(nome, cognome) {
-        this.nome = nome;
-        this.cognome = cognome;
-        this.email = "";
-        this.indirizzo = "";
-    };
-     
-    mostraNomeCompleto() {
-        return this.nome + " " + this.cognome;
+var FormImageManager = {
+    prop1: 0,
+    prop2: 0,
+    
+    get: function()
+    {
+        return this.prop1 + this.prop2;
     }
 };
 
+var myobj1 = Object.create(FormImageManager);
+myobj1.prop1 = 2;
+myobj1.prop2 = 2;
 
-var ImagesManager = new function(param1, param2)
-{
-    this.prop1 = param1;
-    this.prop2 = param2;
-    
-    this.get = function()
-    {
-        return this.prop1 + this.prop2;
-    };
-};
+var myobj2 = Object.create(FormImageManager);
+myobj2.prop1 = 10;
+myobj2.prop2 = 10;
 
+alert(myobj1.get());
+alert(myobj2.get());
 
-var myman = new ImagesManager();
-myman.prop1 = 2;
-myman.prop2 = 2;
