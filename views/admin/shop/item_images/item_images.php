20a3966cdb793233c4f30eb9c443d2da119e5132
lc <info@fareilweb.com>
2016-07-11 19:37:53 +0200

    added new tables to db scheme

--- views/admin/shop/item_images/item_images.php
+++ views/admin/shop/item_images/item_images.php
@@ -1,35 +1,49 @@
 <?php $this->getView('admin/shop/item_images/image_ilk'); ?>
-
 <div class="row">
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
         <h3><?=Lang::$select_images?></h3>
         <!-- Upload Form =============================================== -->
         <form method="post" name="multiple_upload_form" id="multiple_upload_form" enctype="multipart/form-data" action="<?=Config::$web_path?>/Upload/itemImagesProcess/<?=$this->item->item_id?>">
             <input type="hidden" name="image_form_submit" value="1"/>
             <div class="form-group">
-                <!--input type="hidden" name="fk_item_id" value="<?=$this->item->item_id?>" /-->
                 <input type="file" name="images[]" id="images" class="btn btn-default form-control" multiple>
             </div>
         </form>
     </div>
-    
-    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
-        
-            <label><?=Lang::$preview?></label>
-            <div id="images_preview" class="row">
-              
-            </div>
-        
+    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-9">
+        <h3><?=Lang::$preview?></h3>
+        <div id="images_preview">
+
+        </div>
     </div>
-    
-        <!--span class="glyphicon glyphicon-cloud-upload"></span-->
-        <!--?=Lang::$save . " " .Lang::$images;?-->
-        <!-- Upload Form END =========================================== -->
-    
 </div>
 
 <script type="text/javascript">
 jQuery(document).ready(function(){
+    
+    function RemoveImageFromDb(image_id, index){
+        jQuery("#page-preloader").show();
+        jQuery.post("<?=Config::$web_path?>/Admin/removeItemImage", {
+            image_id: image_id
+        }).done(function(result){
+            var res = JSON.parse(result);
+            if(res.status === 1){
+                jQuery("#index"+index).remove();
+                jQuery("#page-preloader").hide();
+            }else{
+                jQuery("#page-preloader").hide();
+                swal({
+                    title: "<?=Lang::$warning?>",
+                    text: "<strong><?=Lang::$delete_fail?></strong>",
+                    type: "error",
+                    html: true
+                });
+            }
+        }).fail(function(err){
+            jQuery("#page-preloader").hide();
+            var result = JSON.parse(result);
+        });
+    }
     
     // AJAX Submit Upload
     jQuery('#images').on('change', function(e){
@@ -52,13 +66,23 @@ jQuery(document).ready(function(){
         });
     });
 
-    // Images Preview - Remove Image From Preview
+    // Remove Image From Preview
     jQuery("#images_preview").on("click", "button.rem", function(e){
-        var index = jQuery(this).attr("data-index"); 
-        jQuery(".preview_image_wrapper#index"+index).remove();
+        e.preventDefault();
+        var index = jQuery(this).attr("data-index");
+        var image_id = jQuery(this).attr("data-image_id");
+        RemoveImageFromDb(image_id, index);
     });
 
-    // Images Preview - Add Image to Item
+    // Remove Image From Item
+    jQuery("#item_images").on("click", "button.rem", function(e){
+        e.preventDefault();
+        var index = jQuery(this).attr("data-index");
+        var image_id = jQuery(this).attr("data-image_id");
+        RemoveImageFromDb(image_id, index);
+    });
+
+    // Add Image to Item Form
     jQuery("#images_preview").on("click", "button.add", function(e){
         var index = jQuery(this).attr("data-index");
         var image_src = jQuery(this).attr("data-src");
@@ -74,7 +98,5 @@ jQuery(document).ready(function(){
         } 
     });
     
-    
-
 });
 </script>
