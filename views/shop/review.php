8d4f9bc9f9acefc9d2b723f052586d5842c00bf9
Luca Cilfone <Luca Cilfone>
2016-08-23 22:50:35 +0200

    upd

--- views/shop/review.php
+++ views/shop/review.php
@@ -1,62 +1,70 @@
 <div class="container-fluid review">
-
     <h2>
         <span class="glyphicon glyphicon-pushpin"></span>
         <strong><?=Lang::$review;?></strong> <?=Lang::$purchase;?>
+
         <a href="" class="btn btn-lg btn-info">
             <span class="glyphicon glyphicon-ok"></span>
             <?=Lang::$confirm_and_pay;?>
         </a>
     </h2>
-    
-    <hr/>
 
+    <div class="row">
         <h3><?=Lang::$products;?></h3>
-
-        <div class="row">
-            <?php foreach($this->cart->items as $item):?>
-                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                    <h4>
-                        <strong><?=Lang::$id;?></strong>
-                        <?=$item->item_id;?>
-                    </h4>
-                    <h4>
-                        <strong><?=Lang::$item_code;?></strong>                        
-                        <?=$item->item_code;?>
-                    </h4>
-                    <h4>
-                        <strong><?=Lang::$item_price;?></strong>
-                        <?=$item->item_price;?>
-                    </h4>
-                    <h4>
-                        <strong><?=Lang::$item_title;?></strong>
-                        <?=$item->item_title;?>
-                    </h4>
-                    <h4>
-                        <strong><?=Lang::$item_short_desc;?></strong>
-                        <?=$item->item_short_desc;?>
-                    </h4>
-                    <h4>
-                        <strong><?=Lang::$quantity;?></strong>
-                        <?=$item->quantity;?>
-                    </h4>
-                    <h4>
-                        <strong><?=Lang::$image;?></strong>
-                        <?php foreach($item->item_images as $image): ?>
+        <hr/>
+        
+        <?php foreach($this->cart->items as $item):?>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                
+                <h4><?=$item->item_title;?></h4>
+                
+                <table>
+                <tbody>
+                    <tr>
+                        <th><strong><?=Lang::$item_code;?></strong></th>                        
+                        <td><?=$item->item_code;?></td>
+                    </tr>
+                    <tr>
+                        <th><strong><?=Lang::$item_price;?></strong></th>
+                        <td><?=$item->item_price;?></td>
+                    </tr>
+                    <tr>
+                        <th><strong><?=Lang::$quantity;?></strong></th>
+                        <td><?=$item->quantity;?></td>
+                    </tr>
+                    <tr>
+                        <th><strong><?=Lang::$image;?></strong></th>
+                        <td><?php foreach($item->item_images as $image): ?>
                             <?php if($image->is_main): ?>
                                 <div style="width: 100%;">
                                     <img style="width: 80%; margin: 10%;" src="<?=Config::$web_path.$image->image_src;?>" alt="<?=$image->image_alt;?>" title="<?=$image->image_title;?>" />
                                 </div>
                             <?php endif;?>
-                        <?php endforeach;?>
-                    </h4>
-                </div>
-            <?php endforeach;?>
+                        <?php endforeach;?></td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        <?php endforeach;?>
+        
+    </div>
+    
+    <br/>
+    <div class="row">   
+        <br/><h3><?=Lang::$shipping;?></h3>
+        <hr/>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <!--$this->cart->shipping_id-->
         </div>
+    </div>
 
-        <?php
-            //$this->cart->shipping_id
-            //$this->cart->payment_id
-        ?>
+    <br/>
+    <div class="row">
+        <br/><h3><?=Lang::$payment;?></h3>
+        <hr/>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <!--$this->cart->payment_id-->
+        </div>
+    </div>
 
 </div>
